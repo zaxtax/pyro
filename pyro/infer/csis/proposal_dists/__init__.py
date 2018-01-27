ff33cb7632646f18d5bea97d1ce3b01fd1ea3332
Will Harvey <williamharvey08@gmail.com>
2018-01-27 22:08:55 +0000

    added categorical proposal dist

--- pyro/infer/csis/proposal_dists/__init__.py
+++ pyro/infer/csis/proposal_dists/__init__.py
@@ -2,7 +2,9 @@ from __future__ import absolute_import, division, print_function
 
 # distribution classes
 from pyro.distributions.random_primitive import RandomPrimitive
+from pyro.distributions.categorical import Categorical
 from pyro.infer.csis.proposal_dists.uniform import UniformProposal
 
 # function aliases
 uniform_proposal = RandomPrimitive(UniformProposal)
+categorical_proposal = RandomPrimitive(Categorical)
